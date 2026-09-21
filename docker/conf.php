38acaa458ea07fd6653f5766762e20beb694dbe1
Ivan <ivan@quad4.io>
2026-09-20 23:46:43 -0500

    Identify the deployment as a Quad4 fork in the footer and metadata
    The about box info text now links to the fork repository and notes that
    the instance is served behind RavenGuard, replacing the upstream
    project page link. Composer and npm metadata point at the fork and use
    a matching description.

--- docker/conf.php
+++ docker/conf.php
@@ -4,6 +4,8 @@
 ; All options: https://github.com/PrivateBin/PrivateBin/wiki/Configuration
 
 [main]
+name = "PrivateBin"
+info = "Hardened <a href='https://github.com/Quad4-Software/PrivateBin'>Quad4 Software fork</a> of PrivateBin, served behind <a href='https://ravenguard.quad4.io/'>RavenGuard</a>."
 discussion = false
 opendiscussion = false
 password = true
